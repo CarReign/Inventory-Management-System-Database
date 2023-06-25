eeecfcc97400874f86640c295f62169914dff45c
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2023-06-26 05:09:17 +0800

    additional updates

--- New/my.sql
+++ New/my.sql
@@ -35,8 +35,7 @@ CREATE TABLE purchase_info (
     product_id INT NOT NULL,
     supplier_id INT NOT NULL,
     purchase_date DATE NOT NULL DEFAULT (CURRENT_TIMESTAMP),
-    quantity INT NOT NULL,
-    total_cost DOUBLE PRECISION NOT NULL,
+    ordered_quantity INT NOT NULL,
     PRIMARY KEY (purchase_no),
     FOREIGN KEY (product_id) REFERENCES products(product_id),
     FOREIGN KEY (supplier_id) REFERENCES suppliers(supplier_id)
@@ -46,37 +45,44 @@ CREATE TABLE IF NOT EXISTS stocks (
     stock_id INT NOT NULL AUTO_INCREMENT,
     purchase_no INT NOT NULL,
     product_id INT NOT NULL,
-    quantity INT NOT NULL,
-    expiration_date DATE NOT NULL,
-    sell_price DOUBLE PRECISION NOT NULL,
+    quantity_received INT NOT NULL,
     buy_price DOUBLE PRECISION NOT NULL,
+    total_cost DOUBLE PRECISION NOT NULL,
+    expiration_date DATE NOT NULL,
+    date_received DATE NOT NULL DEFAULT (CURRENT_TIMESTAMP),
     PRIMARY KEY (stock_id)
 );
 
 -- -------------------------  VIEWS  ------------------------- --
 
-CREATE OR REPLACE VIEW view_stocks 
-AS SELECT 
-    stocks.quantity AS `Stock Quantity`, 
-    stocks.expiration_date AS `Stock Expiration Date`, 
-    stocks.sell_price AS `Stock Selling Price`, 
-    stocks.buy_price AS `Stock Buying Price`, 
+CREATE VIEW IF NOT EXISTS view_stocks AS
+SELECT
+    stocks.quantity_received AS `Quantity Received`,
+    stocks.buy_price AS `Buy Price`,
+    stocks.total_cost AS `Total Cost`,
+    stocks.expiration_date AS `Expiration Date`,
+    stocks.date_received AS `Date Received`,
     products.product_name AS `Product`,
     products.product_description AS `Product Description`,
     categories.category_name AS `Category`,
     categories.category_description AS `Category Description`,
-    stocks.stock_id, 
-    stocks.purchase_no, 
-    stocks.product_id
+    stocks.stock_id AS `Stock Id`,
+    stocks.purchase_no AS `Purchase #`,
+    stocks.product_id AS `Product Id`,
 FROM stocks
 INNER JOIN products ON stocks.product_id = products.product_id
 INNER JOIN categories ON products.product_category_id = categories.category_id;
 
 -- -------------------------  TRIGGERS  ------------------------- --
 
-
 -- -------------------------  PROCEDURES  ------------------------- --
-
 
 -- -------------------------  FUNCTIONS  ------------------------- --
 
+
+-- -------------------------  USERS  ------------------------- --
+CREATE ROLE 'Inventory Manager';
+CREATE ROLE 'Inventory Auditor';
+
+CREATE USER 'CarrenMaeYongco'@'%' IDENTIFIED BY 'CarrenMaeYongco1!';
+CREATE USER 'NinaRoseSebial'@'%' IDENTIFIED BY 'NinaRoseSebial1!';
